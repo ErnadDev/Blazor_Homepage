09eebd5ecda20106519a1fabaf11c68ac1b38bb4
ErnadJ <Ejakupovic@pp2000.com>
2021-11-13 15:34:58 +0100

    Skill Matrix eingebunden. Daten-Aufruf über REST API.

--- Blazor_Homepage/Startup.cs
+++ Blazor_Homepage/Startup.cs
@@ -1,4 +1,3 @@
-using Blazor_Homepage.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +27,7 @@ namespace Blazor_Homepage
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
